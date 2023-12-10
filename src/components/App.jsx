11cebba17b7b09e3331cfe698100994cd308c954
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-12-10 14:11:48 +0200

    commit2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,31 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    const savedFilter = localStorage.getItem('filter');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+    if (savedFilter) {
+      this.setState({ filter: JSON.parse(savedFilter) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+    if (prevState.filter !== this.state.filter) {
+      localStorage.setItem('filter', JSON.stringify(this.state.filter));
+    }
+  }
+
   addContact = (name, number) => {
     const { contacts } = this.state;
-    const isNameExists = contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
+    const isNameExists = contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
 
     if (isNameExists) {
       alert(`${name} is already in contacts!`);
@@ -25,24 +47,24 @@ class App extends Component {
       number,
     };
 
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       contacts: [...prevState.contacts, newContact],
     }));
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== contactId),
     }));
   };
 
-  handleFilterChange = event => {
+  handleFilterChange = (event) => {
     this.setState({ filter: event.target.value });
   };
 
   render() {
     const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
+    const filteredContacts = contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
 
